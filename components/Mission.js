d70c9b94958f795157b75860db175178400fb6a8
sean00880 <theelitereapersanctum@gmail.com>
2022-09-03 02:02:47 -0400

    about

--- components/Mission.js
+++ components/Mission.js
@@ -21,16 +21,18 @@ const Mission = () => {
 
           <div >
             
-          <h1 className={style.teamtext}>Team</h1><hr/>
+          <h1 className={style.teamtext}>Foundation</h1><hr/>
           <h3 className={style.text}>
           <blockquote>Grandpa Ron's Landscaping Services was founded in [2021] by Joshua [and ...] with Granda Ron's vision and values in mind. This company is locally owned and operated and serves Columbus, Ohio and surrounding regions. </blockquote>
           </h3>
           </div>
           <div>
           
-          <h1 className={style.teamtext}>Mission</h1><hr/>
+          <h1 className={style.teamtext}>What We Offer</h1><hr/>
           <h3 className={style.text}>
-          <blockquote>We are experts in residential as well as commercial landscaping and and provide a variety of services inclduing: Tree removal, mulching, mowing, leaf-removal, patio & retaining walls, tree removal services, ice/snow management and more.</blockquote> <br/><blockquote>In order to provide the best possible service, we underpromise and overdeliver. We collaborate closely with our clients and work hard while prioritizing and delivering the highest standards of quality.</blockquote></h3>
+
+          
+          <blockquote>We are experts in commercial and residential landscaping. We provide services such as, mowing, mulching, paver patios, retaining walls, view enhancement pruning, structural pruning, tree removal, fall cleanup, snow and ice management, junk removal, gutter cleaning and more! In order to provide the best service we collaborate closely with our customers to prioritize and deliver the highest quality work.</blockquote></h3>
           
           </div>
           </div>
