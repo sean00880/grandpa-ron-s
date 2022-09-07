05ff88aadd12629f77078e65b939af915a888409
sean00880 <theelitereapersanctum@gmail.com>
2022-09-07 11:32:52 -0400

    chagne

--- data.js
+++ data.js
@@ -155,7 +155,7 @@ export const data = [
       },
       {
         id: 2,
-        url: "https://i.postimg.cc/HnWZZNbS/patio7.jpg",
+        url: "https://i.postimg.cc/1XQ2hZdP/demonew.jpg",
       },
       {
         id: 3,
