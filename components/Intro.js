6835aafffd703b52f99dae6cb47cd0d349372d1b
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-02 08:49:10 -0400

    Update Intro.js

--- components/Intro.js
+++ components/Intro.js
@@ -19,7 +19,7 @@ const Intro = () => {
       <div className={style.card} data-scroll data-scroll-speed="7" data-scroll-delay="0.07">
         <div className={style.content}>
         <h1 className={style.title}>
-          <span className={style.brandName}>Professional</span> Landscaping For All Needs
+          <span className={style.brandName}>Professional</span> Landscaping Services in Canal Winchester, OH
           <hr />
         </h1>
 
@@ -36,16 +36,16 @@ const Intro = () => {
               />
           Grandpa Ron and Joshua mowing the lawn [2000].
         </p>
-        <h2 className={style.subtitle}>Serving Canal Winchester, Ohio<br></br>(220) 666 2520</h2>
+        <h2 className={style.subtitle}>Serving Canal Winchester & Columbus, Ohio<br></br>(220) 666 2520</h2>
         <button className={style.button}><Link href={`${process.env.NEXT_PUBLIC_URL}/#services`} passHref>Services</Link></button>
         </div>
       </div>
       <div className={style.card2}>
       <div className={style.content2}>
-        <h1 className={style.title2}>
+        <h2 className={style.title2}>
           <span className={style.brandName2}>Three</span> Simple Steps:
           <hr />
-        </h1>
+        </h2>
         
         <p className={style.desc2}>
           1) Contact us with a description of your needs and we will provide a free estimate. <br/><br/> 
