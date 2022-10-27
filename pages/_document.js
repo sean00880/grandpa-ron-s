34dae822c36dc36fd6418dde904115f6915cb866
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-27 14:16:15 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,18 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { useRouter } from "next/router";
+
+const CANONICAL_DOMAIN = 'https://yoursite.com';
+
+const router = useRouter();
+const _pathSliceLength = Math.min.apply(Math, [
+    router.asPath.indexOf('?') > 0 ? router.asPath.indexOf('?') : router.asPath.length,
+    router.asPath.indexOf('#') > 0 ? router.asPath.indexOf('#') : router.asPath.length
+]);
+const canonicalURL= CANONICAL_DOMAIN + router.asPath.substring(0, _pathSliceLength);
+
+<Head>
+  
+</Head>
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,7 +24,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <title>Grandpa Ron's | Lawns and Landscape LLC</title>
+        <title>Grandpa Rons | Lawns and Landscape LLC </title>
+        <link rel="canonical" href={ canonicalURL } />
         <meta
           name="description"
           content="Landscaping Services | Canal Winchester, Ohio | Experience the high quality landscaping services by the most preferred landscapers in Canal Winchester, OH. | Serving Canal Winchester & Columbus, OH and surrounding regions.| Family owned & operated | Licensed & Insured. "
